43e63a8291585fcdc4950b9c3e510469f8de2f0e
NickCodes20 <nickcodes20@gmail.com>
2023-02-04 18:13:36 -0600

    Initial commit, refactoring may be necessary

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector(".hamburger").addEventListener("click", function () {
+  document.querySelector(".nav-list").classList.toggle("show")
+})
+
+document.querySelector(".hamburger").addEventListener("click", function () {
+  document.querySelector(".nav-bar-glass").classList.toggle("show")
+})
